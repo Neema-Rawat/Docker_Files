e1c1d348f6bd7e9030de837b5d13ac26e4cf1278
Neema-Rawat <neemarawat781@gmail.com>
2023-07-06 17:28:57 +0530

    Add Files

---
+++ shell/demo1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+date
+sleep 2
+echo "Hey this is Neema"
+sleep 2
+pwd
+sleep 2
+whoami
+sleep 2
+ls
---
+++ task/demo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Hello, Goodmorning"
+sleep 2
+date
+sleep 1
+pwd
